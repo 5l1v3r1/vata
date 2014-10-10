48457c70d042ed5327219ddc4d806e6b33a57d23
Pasika Volodymyr <paccika@gmail.com>
2014-10-10 14:48:34 -0700

    did

--- library/My/View/Helper/Midali.php
+++ library/My/View/Helper/Midali.php
@@ -1,50 +1,26 @@
 <?php
 
-class My_View_Helper_Midali extends Zend_View_Helper_Abstract{
+class My_View_Helper_Midali extends Zend_View_Helper_Abstract
+{
 
 	public function midali($data)
 	{
 
 		$string = "";
 
-		if (count($data) < 3) {
+		$string .= '<div class="row">';
+		foreach ($data as $key => $midal) {
 
-			$string .= '<div class="row">';
+			$string .= '<div class="col-sm-2 col-lg-2 col-md-2">';
+			$string .= "<img class = 'img-responsive img-thumbnail' alt = '' src='/data/img/design/medals/{$midal["image"]}'>";
+			$string .= "</div>";
 
-			foreach ($data as $key => $midal){
-
-				$string .= '<div class="col-sm-3 col-lg-3 col-md-3">';
-				$string .= "<img class = 'img-responsive img-thumbnail' alt = '' src='/data/img/design/medals/{$midal["image"]}'>";
-				$string .= "</div>";
-
-			}
-
-			$string .= '</div>';
-
-		} else {
-
-			$c = 0;
-			$r = 0;
-
-
-			foreach ($data as $key => $midal){
-
-				if (!($r % 3)) $string .= '<div class="row">';
-				$string .= '<div class="col-sm-3 col-lg-3 col-md-3">';
-				$string .= "<img class = 'img-responsive img-thumbnail' alt = '' src='/data/img/design/medals/{$midal["image"]}'>";
-				$string .= "</div>";
-
-				$c++;
-				$r++;
-
-				if (!($r % 3)) $string .= '</div>';
-
-			}
 		}
+		$string .= '</div>';
+
 
 		echo $string;
 
 	}
-
 
 }
