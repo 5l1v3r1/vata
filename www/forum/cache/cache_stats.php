58757c52971aa4f8d00b36ed8041790dfd59acd2
root <root@localhost>
2014-10-23 13:54:17 +0300

    local

--- www/forum/cache/cache_stats.php
+++ www/forum/cache/cache_stats.php
@@ -11,7 +11,7 @@ $forum_stats = array (
   ),
   'total_topics' => '4',
   'total_posts' => '5',
-  'cached' => 1413420272,
+  'cached' => 1413980380,
 );
 
 ?>
--- www/forum/cache/cache_updates.php
+++ www/forum/cache/cache_updates.php
@@ -3,7 +3,7 @@
 if (!defined('FORUM_UPDATES_LOADED')) define('FORUM_UPDATES_LOADED', 1);
 
 $forum_updates = array (
-  'cached' => 1413551809,
+  'cached' => 1413988537,
   'fail' => false,
 );
 
