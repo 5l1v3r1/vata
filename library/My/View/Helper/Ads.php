c32d9e1ff5e33a232b1925623f80690267a89c94
Volodymyr Pasika <vpasika@svitla.com>
2014-11-05 12:03:38 +0200

    test

--- library/My/View/Helper/Ads.php
+++ library/My/View/Helper/Ads.php
@@ -4,17 +4,17 @@ class My_View_Helper_Ads extends Zend_View_Helper_Abstract{
 
 	function ads(){
 
-		#if(APPLICATION_ENV == "testing") return false;
+		if(APPLICATION_ENV == "testing") return false;
 
-		$content = '			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- vatakvadrata -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:250px;height:250px"
-     data-ad-client="ca-pub-2839396741829240"
-     data-ad-slot="8582441812"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>';
+		$content = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+							<!-- vatakvadrata -->
+							<ins class="adsbygoogle"
+							     style="display:inline-block;width:250px;height:250px"
+							     data-ad-client="ca-pub-2839396741829240"
+							     data-ad-slot="8582441812"></ins>
+							<script>
+							(adsbygoogle = window.adsbygoogle || []).push({});
+							</script>';
 
 		echo $content;
 
